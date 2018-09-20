2d7618bf44ab2d4324202ec6a4672d5427dcea6c
mondus <p.richmond@sheffield.ac.uk>
2018-09-20 15:43:34 +0100

    jsGantt views integrated and working

--- rse/models.py
+++ rse/models.py
@@ -1,35 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import timedelta
 
 class PercentageField(models.FloatField):
     #widget = models.TextInput(attrs={"class": "percentInput"})
 
     def to_python(self, value):
         val = super(PercentageField, self).to_python(value)
-        if is_number(val):
+        if val.isnumeric():
             return val/100
         return val
 
     def prepare_value(self, value):
         val = super(PercentageField, self).prepare_value(value)
-        if is_number(val) and not isinstance(val, str):
+        if val.isnumeric() and not isinstance(val, str):
             return str((float(val)*100))
         return val
-
+        
 class SalaryBand(models.Model):
     grade = models.IntegerField(default=1)
     grade_point = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return str(self.grade) + '.' + str(self.grade_point)
     
 class Client(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     
+    def __str__(self):
+        return self.name
+    
 
 class RSE(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
     salary_band = models.ForeignKey(SalaryBand, on_delete=models.DO_NOTHING)
     current_employment = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
     
 class Project(models.Model):
     creator = models.OneToOneField(User, on_delete=models.DO_NOTHING)
@@ -43,7 +53,7 @@ class Project(models.Model):
     
     start = models.DateTimeField()
     end = models.DateTimeField()
-    percentage = PercentageField()
+    percentage = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     
     overheads = models.BooleanField(default=True)
     STATUS_CHOICES = (
@@ -54,9 +64,24 @@ class Project(models.Model):
     )
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     
+    def __str__(self):
+        return self.name
+        
+    @property
+    def duration(self):
+        return (self.end - self.start).days
+        
+    
 class RSEAllocation(models.Model):
     rse = models.ForeignKey(RSE, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    percentage = PercentageField()
+    percentage = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     start = models.DateTimeField()
     end = models.DateTimeField()
+    
+    def __str__(self):
+        return str(self.rse.user) + ' on ' + str(self.project) + ' at ' + str(self.percentage) + '%'
+        
+    @property
+    def duration(self):
+        return (self.end - self.start).days
--- rse/views.py
+++ rse/views.py
@@ -1,5 +1,66 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, get_list_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from datetime import timedelta
+from django.db.models import Max, Min
+
+from .models import *
 
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
+    
+@login_required
+def project_view(request, project_id):
+    # get the project
+    proj = get_object_or_404(Project, pk=project_id)
+    
+    #dict for view
+    dict = {}
+    
+    # get allocations for project
+    allocations = RSEAllocation.objects.filter(project = proj)
+    dict['allocations'] = allocations
+    
+    # calculate commitments
+    dict['proj_days'] = proj.duration
+    dict['committed_days'] = sum(a.duration for a in allocations)
+    
+    # add proj to dict
+    dict['proj'] = proj
+    
+    return render(request, 'project_view.html', dict)
+    
+@login_required
+def rse_view(request, rse_username):
+    # get the user
+    user = get_object_or_404(User, username=rse_username)
+    
+    #dict for view
+    dict = {}
+    
+    #get RSE if exists
+    rse = get_object_or_404(RSE, user=user)
+    dict['rse'] = rse   
+
+    # get allocations for rse
+    allocations = RSEAllocation.objects.filter(rse = rse)
+    dict['allocations'] = allocations
+   
+        
+    return render(request, 'rse_view.html', dict)
+    
+@login_required
+def team_view(request):
+
+    #dict for view
+    dict = {}
+    
+    #get RSE if exists
+    rses = {}
+    for rse in RSE.objects.all():
+        rses[rse] = RSEAllocation.objects.filter(rse=rse)
+    dict['rses'] = rses
+   
+        
+    return render(request, 'team_view.html', dict)
