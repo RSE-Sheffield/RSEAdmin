d878ee613a451ce772a49b7411c82a2f0185fc90
mondus <p.richmond@sheffield.ac.uk>
2018-09-26 16:26:44 +0100

    effort graph working on staff view

--- rse/views.py
+++ rse/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, get_list_or_404, render, redirec
 from django.contrib.auth.decorators import login_required
 from datetime import timedelta
 from django.db.models import Max, Min
+import itertools as it
 
 from .models import *
 
@@ -46,6 +47,20 @@ def rse_view(request, rse_username):
     # get allocations for rse
     allocations = RSEAllocation.objects.filter(rse = rse)
     dict['allocations'] = allocations
+    
+    # create list of start and end dates
+    starts = [[item.start, item.percentage] for item in allocations]
+    ends = [[item.end, -item.percentage] for item in allocations]
+    # sort date events
+    events = sorted(starts + ends, key=lambda x: x[0])
+    # 
+    pdates, deltas = zip(*events)
+    effort = it.accumulate(deltas)
+    plot_events = list(zip(pdates, effort))    
+    
+    dict['plot_events'] = plot_events
+    
+    plot_events = [[e[0], (e[1])] for e in events]
    
         
     return render(request, 'rse_view.html', dict)
