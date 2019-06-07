a0a102f59a7c23ec28fb8497915094f380c13e57
Will Furnass <will@thearete.co.uk>
2019-06-07 17:21:37 +0100

    Ensure Project.funder_id can be null but only if project in prep stage; (+ linting and type hints)

--- rse/tests/test_models.py
+++ rse/tests/test_models.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime
 
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from rse.models import (
@@ -14,7 +15,7 @@ from rse.models import (
         )
 
 
-class SalaryCalcultionTests(TestCase):
+class SalaryCalculationTests(TestCase):
 
     def setUp(self):
         # Create some financial years
@@ -77,43 +78,40 @@ class SalaryCalcultionTests(TestCase):
         # create some test projects
         c = Client(name="test_client")
         c.save()
-        p = Project(creator=self.user, \
-                    created=datetime.now(), \
-                    funder_id="12345", \
-                    name="test_project", \
-                    description="none", \
-                    client=c, \
-                    start=date(2010, 1, 1), \
-                    end=date(2050, 1, 1), \
-                    percentage=50, \
+        p = Project(creator=self.user,
+                    created=datetime.now(),
+                    funder_id="12345",
+                    name="test_project",
+                    description="none",
+                    client=c,
+                    start=date(2010, 1, 1),
+                    end=date(2050, 1, 1),
+                    percentage=50,
                     status='F')
         p.save()
-        a = RSEAllocation(rse=self.rse, project=p, percentage=50, \
-                          start=date(2017, 8, 1), 
+        a = RSEAllocation(rse=self.rse, project=p, percentage=50,
+                          start=date(2017, 8, 1),
                           end=date(2018, 7, 31))
         a.save()
-        a2 = RSEAllocation(rse=self.rse, project=p, percentage=50, \
-                          start=date(2017, 8, 1), 
-                          end=date(2019, 7, 31))
+        a2 = RSEAllocation(rse=self.rse, project=p, percentage=50,
+                           start=date(2017, 8, 1),
+                           end=date(2019, 7, 31))
         a2.save()
 
     def test_salary_year_span(self):
-        """
-        Just a bunch of simple tests to check the financial used to see if dates span the financial year.
-        """
+        """Simple tests to check the financial used to see if dates span the financial year."""
         # Obvious true
-        self.assertEqual(SalaryBand.spansFinancialYear(date(2017, 1, 1), date(2018, 1, 1)), True)
+        self.assertEqual(SalaryBand.spans_financial_year(date(2017, 1, 1), date(2018, 1, 1)), True)
         # True by a long way
-        self.assertEqual(SalaryBand.spansFinancialYear(date(2017, 1, 1), date(2020, 8, 1)), True)
+        self.assertEqual(SalaryBand.spans_financial_year(date(2017, 1, 1), date(2020, 8, 1)), True)
         # One day before true
-        self.assertEqual(SalaryBand.spansFinancialYear(date(2017, 1, 1), date(2017, 7, 31)), False)
+        self.assertEqual(SalaryBand.spans_financial_year(date(2017, 1, 1), date(2017, 7, 31)), False)
         # True by only one day
-        self.assertEqual(SalaryBand.spansFinancialYear(date(2017, 1, 1), date(2017, 8, 1)), True)
+        self.assertEqual(SalaryBand.spans_financial_year(date(2017, 1, 1), date(2017, 8, 1)), True)
 
     def test_salary_increment_final_year(self):
-        """
-        Test will check the incremented salary band for a salary band
-        with no data for the next year.
+        """Check the incremented salary band for a salary band with no data for the next year.
+
         The correct behaviour is that the salary band should be estimated by
         providing the next increment from the current year.
         i.e. G1.1 from 2019 should be incremented to G1.2 2019.
@@ -121,14 +119,13 @@ class SalaryCalcultionTests(TestCase):
         sb11 = SalaryBand.objects.filter(grade=1, grade_point=1, year__year=2019)[0]
         # Sanity check for salary of first 2019 salary band at grade 1.1
         self.assertEqual(sb11.salary, 1002)
-        sb12 = sb11.salaryBandAfterIncrement()
+        sb12 = sb11.salary_band_after_increment()
         # salary grade point should be based of 2019 data as no 2020 data exists
         self.assertEqual(sb12.salary, 2002)
 
     def test_salary_increment_first_year(self):
-        """
-        Test will check the incremented salary band for a salary band
-        with data available for the next financial year.
+        """Check the incremented salary band for a salary band with data available for the next financial year.
+
         The correct behaviour is that the salary band should be incremented
         using the next financial years data
         i.e. G1.1 from 2017 should be incremented to G1.2 2018.
@@ -136,14 +133,13 @@ class SalaryCalcultionTests(TestCase):
         sb11 = SalaryBand.objects.filter(grade=1, grade_point=1, year__year=2017)[0]
         # Sanity check for salary of first 2017 salary band at grade 1.1
         self.assertEqual(sb11.salary, 1000)
-        sb12 = sb11.salaryBandAfterIncrement()
+        sb12 = sb11.salary_band_after_increment()
         # salary grade point should be based of 2018 data
         self.assertEqual(sb12.salary, 2001)
 
     def test_salary_increment(self):
-        """
-        Test will check the that a non incrementing salary band
-        remains on the same grade point but uses the next financial years data.
+        """Check that a non incrementing salary band remains on the same grade point but uses the next financial years data.
+
         The correct behaviour is that the salary band should not be incremented
         but the next years financial data should be used
         i.e. G1.4 from 2017 should be incremented to G1.4 2018.
@@ -151,7 +147,7 @@ class SalaryCalcultionTests(TestCase):
         sb14_2017 = SalaryBand.objects.filter(grade=1, grade_point=4, year__year=2017)[0]
         # Sanity check for salary of first 2017 salary band at grade 1.4
         self.assertEqual(sb14_2017.salary, 4000)
-        sb14_2018 = sb14_2017.salaryBandAfterIncrement()
+        sb14_2018 = sb14_2017.salary_band_after_increment()
         # salary grade point should remain the same but use 2018 data
         self.assertEqual(sb14_2018.salary, 4001)
 
@@ -172,27 +168,27 @@ class SalaryCalcultionTests(TestCase):
         # Check salary for grade point 1.1
         self.assertEqual(sgc.salary_band.salary, 1000)
         # Check salary for rse in same year and same financial year
-        sb = sgc.salaryBandAtFutureDate(date(2017, 12, 12))
+        sb = sgc.salary_band_at_future_date(date(2017, 12, 12))
         self.assertEqual(sb.salary, 1000)
         # Check salary for rse in next year but same financial year
-        sb = sgc.salaryBandAtFutureDate(date(2018, 6, 1))
+        sb = sgc.salary_band_at_future_date(date(2018, 6, 1))
         self.assertEqual(sb.salary, 1000)
         # Check salary for rse at first date in next financial year
         # (should be normal increment using 2018 data)
-        sb = sgc.salaryBandAtFutureDate(date(2018, 8, 1))
+        sb = sgc.salary_band_at_future_date(date(2018, 8, 1))
         self.assertEqual(sb.salary, 2001)
         # Check salary for rse at date in next financial year
         # (should be normal increment using 2018 data)
-        sb = sgc.salaryBandAtFutureDate(date(2018, 12, 12))
+        sb = sgc.salary_band_at_future_date(date(2018, 12, 12))
         self.assertEqual(sb.salary, 2001)
         # Check salary for rse at a date two years in future
         # (should be retrieved from salary band change to g1.5)
-        sb = sgc.salaryBandAtFutureDate(date(2019, 10, 10))
+        sb = sgc.salary_band_at_future_date(date(2019, 10, 10))
         self.assertEqual(sb.salary, 5002)
         # Check salary for rse at a date many years in future
         # (should be retrieved from salary band change to g1.5)
         # which does not increment
-        sb = sgc.salaryBandAtFutureDate(date(2050, 10, 10))
+        sb = sgc.salary_band_at_future_date(date(2050, 10, 10))
         self.assertEqual(sb.salary, 5002)
 
     def test_query_salary_band(self):
@@ -208,8 +204,36 @@ class SalaryCalcultionTests(TestCase):
     def test_allocation_costs(self):
         # test cost of 50% allocation for full 2017 year (expects 1000*0.5)
         a = RSEAllocation.objects.all()[0]
-        self.assertEqual(a.staffCost(), 500)
+        self.assertEqual(a.staff_cost(), 500)
         # Test cost of 50% allocation for full 2017 and 2018 year
         # (expects 1000*.5 + 2001*0.5)
         a = RSEAllocation.objects.all()[1]
-        self.assertEqual(a.staffCost(), 1500.5)
+        self.assertEqual(a.staff_cost(), 1500.5)
+
+    def test_project_(self):
+        """Assert a Project's funder_id can be null unless in preparation."""
+        try:
+            p_good = Project(creator=self.user,
+                             created=datetime.now(),
+                             funder_id=None,
+                             name="test_project",
+                             description="none",
+                             client=Client.objects.all()[0],
+                             start=date(2010, 1, 1),
+                             end=date(2050, 1, 1),
+                             percentage=50,
+                             status='P')
+            p_good.clean()
+        except ValidationError:
+            self.fail("p_good.clean() raised a ValidationError unexpectedly")
+        p_bad = Project(creator=self.user,
+                        created=datetime.now(),
+                        funder_id=None,
+                        name="test_project",
+                        description="none",
+                        client=Client.objects.all()[0],
+                        start=date(2010, 1, 1),
+                        end=date(2050, 1, 1),
+                        percentage=50,
+                        status='F')
+        self.assertRaises(ValidationError, p_bad.clean)
