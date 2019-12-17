1843983fd746d6f188f3d054a25c04204f97fed4
mondus <p.richmond@sheffield.ac.uk>
2019-12-17 12:45:31 +0000

    migrated nativation to project rather than rse app

--- RSEAdmin/settings/base.py
+++ RSEAdmin/settings/base.py
@@ -14,6 +14,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+PROJ_DIR = os.path.join(BASE_DIR, 'RSEAdmin')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -50,7 +51,7 @@ ROOT_URLCONF = 'RSEAdmin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(PROJ_DIR, 'templates')),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
