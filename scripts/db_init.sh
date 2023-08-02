4352ab5eba5c842155f19b483abc556f35f57e9e
Yuliang Weng <59968766+yld-weng@users.noreply.github.com>
2023-08-02 15:40:19 +0100

    fix(db script): create user instead of role!

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -8,17 +8,17 @@ user=django
 database_name=django
 password=django_postgres
 
-# Create the role
-psql -c "create role $user with login password '$password';"
+# Create the user
+psql -c "CREATE USER $user WITH login PASSWORD '$password';"
 
 # Create the database
-createdb --owner=django $database_name
+createdb --owner=$user $database_name
 
 # Give the user permission to create databases.
 # This is required for Django testing:
 # https://docs.djangoproject.com/en/4.0/topics/testing/overview/#the-test-database
-  echo "Granting $user user permission to create databases..."
-  psql -c "ALTER ROLE $user CREATEDB"
-  echo "You might also need to apply migrations."
+echo "Granting $user user permission to create databases..."
+psql -c "ALTER ROLE $user CREATEDB"
+echo "You might also need to apply migrations."
 
 
